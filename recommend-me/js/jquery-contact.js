c53ffb25bfa5f095277ba3d4b0c70378253432b3
Will Boudle <william.boudle@gmail.com>
2022-10-18 23:23:03 -0400

    recommend me

---
+++ recommend-me/js/jquery-contact.js
@@ -0,0 +1,97 @@
+jQuery(document).ready(function() {
+
+
+    $('button[data-loading-text]')
+            .on('click', function() {
+                var btn = $(this);
+                btn.button('loading');
+                setTimeout(function() {
+                    btn.button('reset');
+                }, 3000);
+            });
+
+
+    $('#contactform').submit(function() {
+
+        var action = $(this).attr('action');
+
+        $("#message").slideUp(750, function() {
+            $('#message').hide();
+            $.post(action, {
+                name: $('#name').val(),
+                email: $('#email').val(),
+                subject: $('#subject').val(),
+                msg: $('#msg').val(),
+            },
+                    function(data) {
+                        document.getElementById('message').innerHTML = data;
+                        $('#message').slideDown('slow');
+                        $('#submit').removeAttr('disabled');
+                        if (data.match('success') != null)
+                            $('#contactform').slideUp('slow');
+
+                    }
+            );
+
+        });
+
+        return false;
+
+    });
+	
+	$('#charityform').submit(function() {
+
+        var action = $(this).attr('action');
+
+        $("#message-charity").slideUp(750, function() {
+            $('#message-charity').hide();
+            $.post(action, {
+                name: $('#name').val(),
+                email: $('#email').val(),
+                subject: $('#subject').val(),
+                msg: $('#msg').val(),
+            },
+                    function(data) {
+                        document.getElementById('message-charity').innerHTML = data;
+                        $('#message-charity').slideDown('slow');
+                        $('#submit-charity').removeAttr('disabled');
+                        if (data.match('success') != null)
+                            $('#charityform').slideUp('slow');
+
+                    }
+            );
+
+        });
+
+        return false;
+
+    });
+   
+    $('#register').submit(function() {
+
+        var action = $(this).attr('action');
+
+        $("#message").slideUp(750, function() {
+            $('#message').hide();
+            $.post(action, {
+                name: $('#name').val(),
+                email: $('#email').val(),
+                number: $('#number').val(),
+            },
+                    function(data) {
+                        document.getElementById('message').innerHTML = data;
+                        $('#message').slideDown('slow');
+                        $('#submit').removeAttr('disabled');
+                        if (data.match('success') != null)
+                            $('#register').slideUp('slow');
+
+                    }
+            );
+
+        });
+
+        return false;
+
+    });
+
+});
